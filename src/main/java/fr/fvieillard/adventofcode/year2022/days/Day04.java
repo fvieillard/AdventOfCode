c1cf6b5ee3f727b559dbaed7d429d33103d802d6
François Vieillard <17316849+fvieillard@users.noreply.github.com>
2022-12-08 23:00:46 +0100

    Add 2022, Day 4 - part 2

--- src/main/java/fr/fvieillard/adventofcode/year2022/days/Day04.java
+++ src/main/java/fr/fvieillard/adventofcode/year2022/days/Day04.java
@@ -38,7 +38,20 @@ public class Day04 extends Day2022 {
 
     @Override
     public Object getSolutionPart2() {
-        return null;
+        int counter = 0;
+        Scanner in = new Scanner(getInput()).useDelimiter("[\\n,-]");
+
+        while (in.hasNext()) {
+            int[] range1 = {in.nextInt(), in.nextInt()};
+            int[] range2 = {in.nextInt(), in.nextInt()};
+
+            boolean overlap = !(range1[1] < range2[0] || range1[0] > range2[1]);
+//            System.out.printf("range1: %s, range2: %s - overlap: %s%n",
+//                    Arrays.toString(range1), Arrays.toString(range2), overlap);
+
+            if (overlap) counter++;
+        }
+        return counter;
     }
 
 }
