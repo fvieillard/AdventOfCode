1472f7ccf10ce6679c1e38ec8ccd262815a201ca
François Vieillard <17316849+fvieillard@users.noreply.github.com>
2023-12-09 15:39:28 +0100

    Add 2023, Day 09

---
+++ src/test/java/fr/fvieillard/adventofcode/year2023/days/TestDay09.java
@@ -0,0 +1,20 @@
+package fr.fvieillard.adventofcode.year2023.days;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import fr.fvieillard.adventofcode.common.Day;
+
+public class TestDay09 {
+    @Test
+    void testPart1() {
+        Day dayToTest = new Day09(this.getClass().getResourceAsStream("day_09.txt"));
+        Assertions.assertEquals(114, dayToTest.getSolutionPart1());
+    }
+
+    @Test
+    void testPart2() {
+        Day dayToTest = new Day09(this.getClass().getResourceAsStream("day_09.txt"));
+        Assertions.assertEquals(2, dayToTest.getSolutionPart2());
+    }
+}
